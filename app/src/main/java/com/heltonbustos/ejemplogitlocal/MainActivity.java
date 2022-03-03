86ea7b09d2d82820806da0a4eecaec6baae62bdf
Helton Bustos <heltonmsith@hotmail.com>
2022-03-03 19:51:59 -0300

    Se agrega botón Despedir y Toast asociado

--- app/src/main/java/com/heltonbustos/ejemplogitlocal/MainActivity.java
+++ app/src/main/java/com/heltonbustos/ejemplogitlocal/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     Button btnSaludar;
+    Button btnDespedir;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +18,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btnSaludar = findViewById(R.id.btnSaludar);
+        btnDespedir = findViewById(R.id.btnDespedir);
 
         btnSaludar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Toast.makeText(MainActivity.this, "HOLA MUNDO!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnDespedir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "CHAO MUNDO!", Toast.LENGTH_SHORT).show();
             }
         });
     }
