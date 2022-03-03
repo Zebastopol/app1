561601d8e7921d44e8963faf64b2e2cc784be42a
Helton Bustos <heltonmsith@hotmail.com>
2022-03-03 19:42:02 -0300

    Inicio proyecto, Botón Saludar y Toast

---
+++ app/src/main/java/com/heltonbustos/ejemplogitlocal/MainActivity.java
@@ -0,0 +1,28 @@
+package com.heltonbustos.ejemplogitlocal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnSaludar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnSaludar = findViewById(R.id.btnSaludar);
+
+        btnSaludar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "HOLA MUNDO!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
